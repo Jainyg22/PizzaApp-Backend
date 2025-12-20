a0adad2c94f21d4e098962ed30388f01cebeba38
Chirag Jain <jainc451@gmail.com>
2025-12-20 12:55:02 +0530

    Added body parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express'); // function return krega
+const bodyParser = require('body-parser');
 
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
 
 const app = express(); // function ne object return kra jise hum configure kr skte hain
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
+
+
 
 app.listen(ServerConfig.PORT,async ()=>{
     await connectDB();
