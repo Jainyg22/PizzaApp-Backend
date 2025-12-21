a427066b69d4ba08379d3b4716c2c51a6757436a
Chirag Jain <jainc451@gmail.com>
2025-12-21 22:29:28 +0530

    Image upload setup done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const userRouter = require('./routes/userRoute');
 const cartRouter = require('./routes/cartRoute');
 const authRouter = require('./routes/authRoute');
 const { isLoggedIn } = require('./validation/authValidator');
+const uploader = require('./middlewares/multerMiddleware');
+const cloudinary = require('./config/cloudinaryConfig');
+const fs = require('fs/promises');
 // const User = require('./schema/userSchema');
 
 const app = express(); // function ne object return kra jise hum configure kr skte hain
@@ -28,6 +31,14 @@ app.get('/ping',isLoggedIn,(req,res)=>{
     console.log(req.body);
     console.log(req.cookies);
     return res.json({message : 'OK'});
+});
+
+app.post('/photo', uploader.single('incomingFile'),async (req,res)=>{
+    console.log(req.file);
+    const result = await cloudinary.uploader.upload(req.file.path);
+    console.log("Result form cloudinary",result);
+    await fs.unlink(req.file.path);
+    return res.json({ message : 'OK'})
 })
 
 app.listen(ServerConfig.PORT,async ()=>{
