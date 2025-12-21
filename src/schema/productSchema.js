48881a4ddb429f8985b284644ab6e5889ecf6d10
Chirag Jain <jainc451@gmail.com>
2025-12-21 21:06:07 +0530

    Added Product Schema

---
+++ src/schema/productSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const productSchema=new mongoose.Schema({
+    productName: {
+        type : String,
+        // lowercase : true,
+        trim : true,
+        required : [true, "Product name is mandatory"],
+        minlength : [5, "Product name is at least 5 characters long"]
+    },
+    description : {
+        type : String,
+        maxlength : [200, "Maximum length is 200 characters long"],
+        lowercase : true,
+        trim : true,
+    },
+    image : {
+        type : String,
+        // required : [true,"Image is mandatory"]
+    },
+    price:{
+        type : Number,
+        required : [true, "Product price is mandatory"],
+    },
+    category:{
+        type : String,
+        enum : ['veg','non-veg','drinks','sides'],
+        default : 'veg',
+    },
+    inStock:{
+        type : Boolean,
+        required : [true, "In Stock status is required"],
+        default : true
+    }
+},{
+    timestamps : true
+})
+
+const Product = mongoose.model("Product",productSchema);
+
+module.exports = Product;
