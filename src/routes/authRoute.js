5206380dbe0b8ad0f28ff8738ebee40dbbb5b1df
Chirag Jain <jainc451@gmail.com>
2025-12-24 13:07:36 +0530

    Added user order apis and logout

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,6 @@
 // Resource -> User
 const express = require('express');
-const { login } = require("../controllers/authController");
+const { login,logout } = require("../controllers/authController");
 
 
 //We have to initialise a router object to add routes in a new file
@@ -8,5 +8,6 @@ const { login } = require("../controllers/authController");
 const authRouter=express.Router();
 
 authRouter.post('/login',login); // this is a route registration
+authRouter.post('/logout', logout);
 
 module.exports = authRouter; 
